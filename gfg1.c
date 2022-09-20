820b198e3f966ac70434a80e12e67f58382e53c9
Prachi2780 <prachipragnyapadhi@gmail.com>
2022-09-20 12:34:28 +0530

    commit try

---
+++ gfg1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+int search(int arr[],int n,int target)
+{
+    if(n<0)
+    {
+        return 0;
+    }
+    if(arr[n]==target)
+    {
+        return 1;
+    }
+    return search(arr,n-1,target);
+}
+int main()
+{
+    int n,target,i,k,arr[100];
+    printf("enter the target and size of array\n");
+    scanf("%d %d",&target,&n);
+    printf("enter the array\n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    k=search(arr,n,target);
+    printf("%d\n",k);
+    printf(k==0?"not present":"present");
+    
+}
